bf674bde58cff51cc5a8b4c63111b5cfc8c0b56d
wahidpakistani <wahidpakistani@users.noreply.github.com>
2020-06-04 20:47:08 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1> Hello Pakistan</h1>
+    </div>
+  );
+}
+
+export default App;
+ 
